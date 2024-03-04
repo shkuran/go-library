53abba8383a835cd39fc603edf5f0d9a2ab423ee
Mykhailo Shkuran <capgemini.shkuran@extaccount.com>
2024-03-04 17:11:21 +0100

    add tests

--- test/reservation/handler_test.go
+++ test/reservation/handler_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -13,6 +14,24 @@ import (
 	"github.com/shkuran/go-library/reservation"
 	test "github.com/shkuran/go-library/test/book"
 )
+
+func setupTestEnv(booksInDB []book.Book, reservationsInDB []reservation.Reservation) TestEnv {
+	bookRepo := test.NewMockBookRepo(booksInDB)
+	resRepo := NewMockReservationRepo(reservationsInDB)
+	resHandler := reservation.NewHandler(resRepo, bookRepo)
+
+	return TestEnv{
+		BookRepo:           bookRepo,
+		ReservationRepo:    resRepo,
+		ReservationHandler: resHandler,
+	}
+}
+
+type TestEnv struct {
+	BookRepo           *test.MockBookRepo
+	ReservationRepo    *MockReservationRepo
+	ReservationHandler reservation.Handler
+}
 
 func TestGetReservations(t *testing.T) {
 
@@ -46,14 +65,12 @@ func TestGetReservations(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.testName, func(t *testing.T) {
-
-			book_repo := test.NewMockBookRepo(tc.booksInDB)
-			res_repo := NewMockReservationRepo(tc.reservationsInDB)
-			res_handler := reservation.NewHandler(res_repo, book_repo)
+			// Set up the test environment
+			env := setupTestEnv(tc.booksInDB, tc.reservationsInDB)
 
 			router := gin.Default()
 
-			router.GET("/reservations", res_handler.GetReservations)
+			router.GET("/reservations", env.ReservationHandler.GetReservations)
 
 			// Perform a test request
 			req, err := http.NewRequest("GET", "/reservations", nil)
@@ -110,7 +127,7 @@ func TestAddReservation(t *testing.T) {
 		{
 			testName:         "Successfull added reservation",
 			booksInDB:        []book.Book{{ID: 1, Title: "Book_1", AvailableCopies: 1}},
-			reservationsInDB: []reservation.Reservation{{ID: 1, BookId: 1, UserId: 1}},
+			reservationsInDB: []reservation.Reservation{},
 			requestBody:      `{"book_id": 1}`,
 			expectedCode:     http.StatusCreated,
 			expectedErrorMsg: "",
@@ -145,10 +162,8 @@ func TestAddReservation(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.testName, func(t *testing.T) {
-
-			book_repo := test.NewMockBookRepo(tc.booksInDB)
-			res_repo := NewMockReservationRepo(tc.reservationsInDB)
-			res_handler := reservation.NewHandler(res_repo, book_repo)
+			// Set up the test environment
+			env := setupTestEnv(tc.booksInDB, tc.reservationsInDB)
 
 			// HTTP request
 			req := httptest.NewRequest(http.MethodPost, "/reservations", strings.NewReader(tc.requestBody))
@@ -159,10 +174,14 @@ func TestAddReservation(t *testing.T) {
 			gin.SetMode(gin.TestMode)
 			context, _ := gin.CreateTestContext(w)
 			context.Request = req
-			context.Set("userId", 1)
+			context.Set("userId", int64(1))
+			context.Next()
+
+			userId := context.GetInt64("userId")
+			fmt.Println(userId)
 
 			// Perform the request
-			res_handler.AddReservation(context)
+			env.ReservationHandler.AddReservation(context)
 
 			if w.Code != tc.expectedCode {
 				t.Errorf("Expected status %d; got %d", tc.expectedCode, w.Code)
@@ -170,7 +189,7 @@ func TestAddReservation(t *testing.T) {
 
 			if tc.expectedErrorMsg == "" {
 				// Check if AvailableCopies of book with id:1 was updated(was: 1, should be: 0)
-				reservedBook, err := book_repo.GetById(1)
+				reservedBook, err := env.BookRepo.GetById(1)
 				if err != nil {
 					t.Errorf("Could not fetch book! error: %d", err)
 				}
@@ -179,7 +198,7 @@ func TestAddReservation(t *testing.T) {
 				}
 				// Check if reservation was added
 				expRes := reservation.Reservation{ID: 1, BookId: 1, UserId: 1}
-				gotedRes, err := res_repo.GetById(1)
+				gotedRes, err := env.ReservationRepo.GetById(1)
 				if err != nil {
 					t.Errorf("Could not fetch book! error: %d", err)
 				}
@@ -204,3 +223,105 @@ func TestAddReservation(t *testing.T) {
 
 }
 
+func TestCompleteReservation(t *testing.T) {
+	testCases := []struct {
+		testName         string
+		booksInDB        []book.Book
+		reservationsInDB []reservation.Reservation
+		reservationId    int
+		expectedCode     int
+		expectedErrorMsg string
+	}{
+		// Case 1: CopleteReservation add return date for reservation and update AvailableCopies for book
+		{
+			testName:         "Successfull completed reservation",
+			booksInDB:        []book.Book{{ID: 1, Title: "Book_1", AvailableCopies: 1}},
+			reservationsInDB: []reservation.Reservation{{ID: 1, BookId: 1, UserId: 1, ReturnDate: nil}},
+			expectedCode:     http.StatusOK,
+			expectedErrorMsg: "",
+		},
+		// Case 2: CopleteReservation returns a bad request
+		// {
+		// 	testName:         "Bad request",
+		// 	booksInDB:        []book.Book{},
+		// 	reservationsInDB: []reservation.Reservation{},
+		// 	expectedCode:     http.StatusBadRequest,
+		// 	expectedErrorMsg: "Could not parse request data!",
+		// },
+		// // Case 3: CopleteReservation could not fetch book! Returns InternalServerError
+		// {
+		// 	testName:         "No books",
+		// 	booksInDB:        []book.Book{},
+		// 	reservationsInDB: []reservation.Reservation{},
+		// 	expectedCode:     http.StatusInternalServerError,
+		// 	expectedErrorMsg: "Could not fetch book!",
+		// },
+		// // Case 4: CopleteReservation returns a bad request. The book is not available!
+		// {
+		// 	testName:         "AvailableCopies is 0",
+		// 	booksInDB:        []book.Book{{ID: 1, Title: "Book_1", AvailableCopies: 0}},
+		// 	reservationsInDB: []reservation.Reservation{},
+		// 	expectedCode:     http.StatusBadRequest,
+		// 	expectedErrorMsg: "The book is not available!",
+		// },
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			// Set up the test environment
+			env := setupTestEnv(tc.booksInDB, tc.reservationsInDB)
+
+			// HTTP request
+			req := httptest.NewRequest(http.MethodPost, "/reservations", nil)
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			// Gin context
+			gin.SetMode(gin.TestMode)
+			context, _ := gin.CreateTestContext(w)
+			context.Request = req
+			context.Set("userId", int64(1))
+			context.AddParam("id", "1")
+
+			// Perform the request
+			env.ReservationHandler.CopleteReservation(context)
+
+			if w.Code != tc.expectedCode {
+				t.Errorf("Expected status %d; got %d", tc.expectedCode, w.Code)
+			}
+
+			if tc.expectedErrorMsg == "" {
+				// Check if AvailableCopies of book with id:1 was updated(was: 1, should be: 2)
+				reservedBook, err := env.BookRepo.GetById(1)
+				if err != nil {
+					t.Errorf("Could not fetch book! error: %d", err)
+				}
+				if reservedBook.AvailableCopies != 2 {
+					t.Errorf("Expected AvailableCopies %d; got %d", 2, reservedBook.AvailableCopies)
+				}
+				// Check if reservation was added
+				expRes := reservation.Reservation{ID: 1, BookId: 1, UserId: 1}
+				gotedRes, err := env.ReservationRepo.GetById(1)
+				if err != nil {
+					t.Errorf("Could not fetch reservation! error: %d", err)
+				}
+				if !reflect.DeepEqual(gotedRes, expRes) {
+					t.Errorf("Expected new rservation: %v; got: %v", expRes, gotedRes)
+					// t.Errorf("Expected new rservation id:%d, book_id:%d, user_id:%d; got id:%d, book_id:%d, user_id:%d",
+					// 	expRes.ID, expRes.BookId, expRes.UserId, gotedRes.ID, gotedRes.BookId, gotedRes.UserId)
+				}
+			} else {
+				// Check if the response contains the expected error message
+				var response map[string]string
+				err := json.Unmarshal(w.Body.Bytes(), &response)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if response["message"] != tc.expectedErrorMsg {
+					t.Errorf("Expected error message '%s'; got '%s'", tc.expectedErrorMsg, response["message"])
+				}
+
+			}
+		})
+	}
+
+}
